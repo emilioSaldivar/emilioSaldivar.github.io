98deae36a3348aed6c766780e4a45419a521fc2b
emilioSaldivar <emiliomatasc@fpuna.edu.py>
2021-12-09 22:58:35 -0300

    Agregamos efecto scroll izquierda y nuevos proyectos webs

--- js/efectoScroll.js
+++ js/efectoScroll.js
@@ -1,12 +1,17 @@
 let animado = document.querySelectorAll(".animado");//seleccionamos el contenedor
 
 function mostrarScroll() {
-    let scrollTop = document.documentElement.scrollTop;// cantidad de scroll que bajamos
+    let scrollTop = document.documentElement.scrollTop;// cantidad de scroll que hay en la pantalla
     for (var i = 0; i < animado.length; i++){//por cada elemento
         let alturaAnimado = animado[i].offsetTop;//la altura desde la parte superior hasta el elemento
         if( alturaAnimado - 450 < scrollTop ){//para mostrar hasta llegar a la altura
-            animado[i].style.opacity = 1;
-            animado[i].classList.add("mostrarDesdeArriba");
+            if(i < animado.length-2){
+                animado[i].style.opacity = 1;
+                animado[i].classList.add("mostrarDesdeArriba");
+            }else{// se muestran los dos ultimos elemntos que son los proyectos webs
+                animado[i].style.opacity = 1;
+                animado[i].classList.add("mostrarDesdeIzquierda");
+            }
         }
     }
 }
